b3280fe5d9e6b006ecd3ac961c5a3d4e751ce008
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2023-12-21 13:30:31 +0530

    splash screen created

---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset("asset/animation/Animation - 1703144519437.json",
+            fit: BoxFit.cover),
+      ),
+    );
+  }
+}
